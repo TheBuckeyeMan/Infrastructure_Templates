777b4ad411d335a749fbcf9cf9d3be9ce59edafb
TheBuckeyeMan <156697236+TheBuckeyeMan@users.noreply.github.com>
2024-08-30 15:45:19 -0400

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,4 @@
+#Ensure both AWS_ACCESS_KEY and AWS_SECRET_KEY Repo Secrets are properly configured in your repo for CICD Deployment
 provider "aws" {
     region = "us-east-2"
     access_key  = var.aws_access_key
