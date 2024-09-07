d394fdd15534b87cbdaf1dfe4a7b2773d1ec06e1
Adam Maas <adamjm1220@gmail.com>
2024-09-07 11:21:08 -0400

    LambdaUpdate

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,3 +29,36 @@ resource "aws_lambda_function_url" "lambda_url" {
 
   authorization_type = "NONE"
 }
+
+
+#IF USING EVENT BRIDGE TO TRIGGER LAMBDA
+
+#get event bridge rule for lambda - IF ALREADY EXISTS
+#IMPORT EVENT BRIDGE RULE
+data "aws_cloudwatch_event_rule" "existing_event_rule" {
+  name = "DailyEventBridgeTrigger"  # This should match the name of the EventBridge rule from the other repo
+}
+
+#ASSOCIATE LAMBDA WITH EVENT BRIDGE
+#Utilize existing event bridge rule to trigger lambda 1 time per day
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = data.aws_cloudwatch_event_rule.existing_event_rule.name  # Reference the existing rule
+  arn       = aws_lambda_function.api_lambda.arn
+}
+
+#PERMISSIONS
+#Giving permission for event bridge to invoke the lambda function
+# Permission for EventBridge to invoke the Lambda function
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowEventBridgeInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = data.aws_cloudwatch_event_rule.existing_event_rule.arn
+}
+
+
+
+
+
+
